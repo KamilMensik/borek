efbd2ea794c1de12c0b8cdc98b460002a26a8c8f
Kamil Mensik <kamilekmensik@gmail.com>
2025-06-09 09:42:26 +0200

    Modify base shader

--- Borek/assets/shaders/2DBaseShader.glsl
+++ Borek/assets/shaders/2DBaseShader.glsl
@@ -38,16 +38,15 @@ void main()
         vec4 texColor = e_Color;
 
         switch(int(e_TextureId)) {
-                case  0: texColor *= texture(u_Textures[ 0], e_TexCord); break;
-                case  1: texColor *= texture(u_Textures[ 1], e_TexCord); break;
-                case  2: texColor *= texture(u_Textures[ 2], e_TexCord); break;
-                case  3: texColor *= texture(u_Textures[ 3], e_TexCord); break;
-                case  4: texColor *= texture(u_Textures[ 4], e_TexCord); break;
-                case  5: texColor *= texture(u_Textures[ 5], e_TexCord); break;
-                case  6: texColor *= texture(u_Textures[ 6], e_TexCord); break;
-                case  7: texColor *= texture(u_Textures[ 7], e_TexCord); break;
-                case  8: texColor *= texture(u_Textures[ 8], e_TexCord); break;
-                case  9: texColor *= texture(u_Textures[ 9], e_TexCord); break;
+                case 0: texColor *= texture(u_Textures[0], e_TexCord); break;
+                case 1: texColor *= texture(u_Textures[1], e_TexCord); break;
+                case 2: texColor *= texture(u_Textures[2], e_TexCord); break;
+                case 3: texColor *= texture(u_Textures[3], e_TexCord); break;
+                case 5: texColor *= texture(u_Textures[ 5], e_TexCord); break;
+                case 6: texColor *= texture(u_Textures[6], e_TexCord); break;
+                case 7: texColor *= texture(u_Textures[7], e_TexCord); break;
+                case 8: texColor *= texture(u_Textures[8], e_TexCord); break;
+                case 9: texColor *= texture(u_Textures[9], e_TexCord); break;
                 case 10: texColor *= texture(u_Textures[10], e_TexCord); break;
                 case 11: texColor *= texture(u_Textures[11], e_TexCord); break;
                 case 12: texColor *= texture(u_Textures[12], e_TexCord); break;
